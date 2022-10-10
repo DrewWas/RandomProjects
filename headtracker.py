b4693439c28faf83bff6d7ce053571a22055d185
Andrew Wasielewski <awwasielewski@gmail.com>
2022-10-09 23:50:08 -0700

    numero uno

---
+++ headtracker.py
@@ -0,0 +1,6 @@
+"""Shows a window that rotates with your head
+
+ -> If your head is at a 90degree angle, the window rotates to 90degrees.
+ -> Could be nice for watching media while in bed or on couch
+
+"""
